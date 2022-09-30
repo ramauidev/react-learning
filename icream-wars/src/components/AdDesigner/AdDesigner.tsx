2352fa970afaa8f622e1d62de5c70358c79b8838
Rama <ramau07@gmail.com>
2022-09-30 00:37:23 -0400

    updating ice creams wars assignment

--- icream-wars/src/components/AdDesigner/AdDesigner.tsx
+++ icream-wars/src/components/AdDesigner/AdDesigner.tsx
@@ -1,37 +1,92 @@
+import { useState } from "react";
 import "./AdDesigner.css";
 
 const AdDesigner = () => {
+  const [fontsize, setFontsize] = useState(30);
+  const [flavor, setflavor] = useState("Choclate");
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  function assignflavor(name: string) {
+    setflavor(name);
+  }
+
+  function fontsizeUp() {
+    setFontsize((prev) => prev + 1);
+  }
+
+  function fontsizeDown() {
+    setFontsize((prev) => prev - 1);
+  }
+
+  let flavorFontsize = {
+    fontSize: fontsize + "px",
+  };
+
   return (
     <div className="AdDesigner">
       <h2>Ad Designer</h2>
-      <section className="add dark">
+      <section className={"add " + (darkTheme ? "dark" : "light")}>
         <h3>Vote For</h3>
-        <p>Choclate</p>
+        <p style={flavorFontsize}>{flavor}</p>
       </section>
 
       <section>
         <h3>What to Support</h3>
         <div>
-          <button type="button">Choclate</button>
-          <button type="button">Vanila</button>
-          <button type="button">Strawberry</button>
+          <button
+            type="button"
+            onClick={() => assignflavor("Choclate")}
+            disabled={flavor === "Choclate"}
+          >
+            Choclate
+          </button>
+          <button
+            type="button"
+            onClick={() => assignflavor("Vanila")}
+            disabled={flavor === "Vanila"}
+          >
+            Vanila
+          </button>
+          <button
+            type="button"
+            onClick={() => assignflavor("Strawberry")}
+            disabled={flavor === "Strawberry"}
+          >
+            Strawberry
+          </button>
         </div>
       </section>
 
       <section>
         <h3>Color Theme</h3>
         <div>
-          <button type="button">Light</button>
-          <button type="button">Dark</button>
+          <button
+            type="button"
+            onClick={() => setDarkTheme(false)}
+            disabled={!darkTheme}
+          >
+            Light
+          </button>
+          <button
+            type="button"
+            onClick={() => setDarkTheme(true)}
+            disabled={darkTheme}
+          >
+            Dark
+          </button>
         </div>
       </section>
 
       <section>
         <h3>Font Size</h3>
         <div>
-          <button type="button">Down</button>
-          <p>56</p>
-          <button type="button">Up</button>
+          <button type="button" onClick={fontsizeDown}>
+            Down
+          </button>
+          <span className="font-size-text">{fontsize}</span>
+          <button type="button" onClick={fontsizeUp}>
+            Up
+          </button>
         </div>
       </section>
     </div>
--- icream-wars/src/components/Votes/Votes.tsx
+++ icream-wars/src/components/Votes/Votes.tsx
@@ -1,38 +1,112 @@
+import { useState } from "react";
 import "./Votes.css";
 
 const Votes = () => {
+  const [totalVotes, setTotalVotes] = useState(0);
+  const [choclateCount, setChoclateCount] = useState(0);
+  const [vanilaCount, setVanilaCount] = useState(0);
+  const [strawberryCount, setStrawberryCount] = useState(0);
+
+  function updateVote(flavorType: string) {
+    setTotalVotes((vote) => vote + 1);
+    switch (flavorType) {
+      case "Choclate":
+        setChoclateCount((vote) => vote + 1);
+        break;
+      case "Vanila":
+        setVanilaCount((vote) => vote + 1);
+        break;
+      case "Strawberry":
+        setStrawberryCount((vote) => vote + 1);
+        break;
+    }
+  }
+
+  let flavorPercentage = (flavor: number): string => {
+    if (flavor) {
+      return ((flavor / totalVotes) * 100).toFixed(2) + "%";
+    } else {
+      return 0 + "%";
+    }
+  };
+
+  let flavorWidth = (flavor: number) => {
+    return {
+      width: flavorPercentage(flavor),
+    };
+  };
+
   return (
     <div className="Votes">
       <h2>Vote Here</h2>
       <div>
-        <button type="button">Choclate</button>
-        <button type="button">Vanila</button>
-        <button type="button">Strawberry</button>
+        <button type="button" onClick={() => updateVote("Choclate")}>
+          Choclate
+        </button>
+        <button type="button" onClick={() => updateVote("Vanila")}>
+          Vanila
+        </button>
+        <button type="button" onClick={() => updateVote("Strawberry")}>
+          Strawberry
+        </button>
       </div>
-      <div className="choclate">
-        <div>
-          <span>Choclate:</span>
-          <span>2(33.3%)</span>
-        </div>
 
-        <div className="progress"></div>
-      </div>
-      <div className="vanila">
+      {totalVotes === 0 ? (
+        <p>No votes yet.</p>
+      ) : (
         <div>
-          <span>Vanila:</span>
-          <span>1(16.7%)</span>
-        </div>
+          {choclateCount === 0 ? (
+            ""
+          ) : (
+            <div className="flavors choclate">
+              <div>
+                <span>Choclate: </span>
+                <span className="vote-count">
+                  {choclateCount} ({flavorPercentage(choclateCount)})
+                </span>
+              </div>
 
-        <div className="progress"></div>
-      </div>
-      <div className="strawberry">
-        <div>
-          <span>Strawberry:</span>
-          <span>3(50.0%)</span>
-        </div>
+              <div
+                className="progress"
+                style={flavorWidth(choclateCount)}
+              ></div>
+            </div>
+          )}
 
-        <div className="progress"></div>
-      </div>
+          {vanilaCount === 0 ? (
+            ""
+          ) : (
+            <div className="flavors vanila">
+              <div>
+                <span>Vanila: </span>
+                <span className="vote-count">
+                  {vanilaCount} ({flavorPercentage(vanilaCount)})
+                </span>
+              </div>
+
+              <div className="progress" style={flavorWidth(vanilaCount)}></div>
+            </div>
+          )}
+
+          {strawberryCount === 0 ? (
+            ""
+          ) : (
+            <div className="flavors strawberry">
+              <div>
+                <span>Strawberry: </span>
+                <span className="vote-count">
+                  {strawberryCount} ({flavorPercentage(strawberryCount)})
+                </span>
+              </div>
+
+              <div
+                className="progress"
+                style={flavorWidth(strawberryCount)}
+              ></div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
